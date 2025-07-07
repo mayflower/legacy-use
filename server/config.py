c0015770b82d5d0f51c5af50f4722d8c2f768d32
Richard <richard@stromer.org>
2025-07-07 13:54:42 +0200

    read settings from env file

--- server/config.py
+++ server/config.py
@@ -1,9 +1,16 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+
+ENV_FILE_PATH = ROOT_DIR / '.env'
 
 
 class Settings(BaseSettings):
     app_name: str = 'Awesome API'
     items_per_user: int = 50
+
+    model_config = SettingsConfigDict(env_file=ENV_FILE_PATH)
 
 
 settings = Settings()
