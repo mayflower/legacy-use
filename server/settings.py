c0f2a0cb3a16765d1380f40ca3063553c4894de5
Richard <richard@stromer.org>
2025-07-07 14:04:36 +0200

    add api provider and aws env variables to settings

--- server/settings.py
+++ server/settings.py
@@ -1,6 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
+from server.computer_use.config import APIProvider
+
 ROOT_DIR = Path(__file__).parent.parent
 ENV_FILE_PATH = ROOT_DIR / '.env'
 
@@ -8,6 +10,12 @@ ENV_FILE_PATH = ROOT_DIR / '.env'
 class Settings(BaseSettings):
     app_name: str = 'Awesome API'
     items_per_user: int = 50
+
+    API_PROVIDER: APIProvider
+
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: str
+    AWS_REGION: str
 
     model_config = SettingsConfigDict(
         env_file=ENV_FILE_PATH,
