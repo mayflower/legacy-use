6379f76e678e36f5f15532f0f6d8819188f135b6
Richard <richard@stromer.org>
2025-07-07 14:02:42 +0200

    remove environs code, replaced by pydantic settings

--- server/settings.py
+++ server/settings.py
@@ -1,18 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
-from environs import Env
 
 ROOT_DIR = Path(__file__).parent.parent
 ENV_FILE_PATH = ROOT_DIR / '.env'
-
-print('Root dir', ROOT_DIR)
-
-env = Env()
-env.read_env(ENV_FILE_PATH)
-
-print('API_KEY', env('API_KEY'))
-
-print('Env', env.dump())
 
 
 class Settings(BaseSettings):
