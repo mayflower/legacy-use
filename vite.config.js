62ae0b5c905d25f13463bd23f25a2ca9334c3d66
Richard <richard@stromer.org>
2025-07-07 18:00:11 +0200

    remove custom port override

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
     outDir: 'build',
   },
   server: {
-    port: 3000,
     host: '0.0.0.0',
     watch: {
       ignored: ['**/.venv/**', '**/.git/**', '**/server/**', '**/node_modules/**'],
