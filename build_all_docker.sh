310614577c3a5363d70e3785fb709d7486b7a981
Sebastian Schuon <schuon@users.noreply.github.com>
2025-07-10 21:49:43 +0200

    Reduced the size of the images

--- build_all_docker.sh
+++ build_all_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 docker build -t legacy-use-mgmt:local -f infra/docker/legacy-use-mgmt/Dockerfile .
+docker build -t legacy-use-frontend:local -f infra/docker/legacy-use-frontend/Dockerfile .
+docker build -t legacy-use-backend:local -f infra/docker/legacy-use-backend/Dockerfile .
 docker build -t legacy-use-target:local -f infra/docker/legacy-use-target/Dockerfile .
