8673ede6d910df9bde8cb78e890f2e80dd73f5ae
Thilo Reintjes <thilo.reintjes@gmail.com>
2025-07-11 18:40:01 +0200

    Feat: update log message for insufficient credits to reflect API credits exceeded

--- app/components/LogViewer.jsx
+++ app/components/LogViewer.jsx
@@ -290,8 +290,8 @@ const LogViewer = ({ logs }) => {
             return log.content;
           }
         }
-        if (log.content?.message?.includes('insufficient credits')) {
-          return "You've run out of credits. Want to add more? <a href='https://legacy-use.com/'>Book a demo with us!</a>";
+        if (log.content?.includes('API Credits Exceeded')) {
+          return "You've run out of credits...\nBook a demo with us -> https://legacy-use.com/";
         }
         if (typeof log.content === 'string') {
           return log.content;
