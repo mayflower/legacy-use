addd97def446b6d696c8a44b498c43ea4d037e9c
Richard <richard@stromer.org>
2025-07-23 15:51:52 +0200

    add test case to expect 401

--- server/server_test.py
+++ server/server_test.py
@@ -1,8 +1,18 @@
+from fastapi import HTTPException
 from fastapi.testclient import TestClient
+import pytest
 
 from .server import app
 
 client = TestClient(app)
+
+
+def test_main_no_api_key():
+    with pytest.raises(HTTPException):
+        response = client.get('/')
+
+        assert response.status_code == 401
+        assert response.text == 'API key is missing'
 
 
 def test_main():
