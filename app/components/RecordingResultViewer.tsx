20d990305210464dbfcfe5bf66fabbdb11465500
Richard <richard@stromer.org>
2025-07-31 00:03:03 +0200

    show video result as popover

--- app/components/RecordingResultViewer.tsx
+++ app/components/RecordingResultViewer.tsx
@@ -1,4 +1,6 @@
-import { Box } from '@mui/material';
+import { VideoFile } from '@mui/icons-material';
+import { Box, Button, Popover } from '@mui/material';
+import PopupState, { bindPopover, bindTrigger } from 'material-ui-popup-state';
 import type { RecordingResultResponse } from '../gen/endpoints';
 
 export default function RecordingResultViewer({
@@ -7,15 +9,30 @@ export default function RecordingResultViewer({
   recordingResult: RecordingResultResponse;
 }) {
   return (
-    <Box>
-      <video
-        controls
-        style={{ maxWidth: '100%', maxHeight: '200px' }}
-        src={`data:video/mp4;base64,${recordingResult.base64_video}`}
-      >
-        <track kind="captions" srcLang="en" label="English captions" />
-        Your browser does not support the video tag.
-      </video>
-    </Box>
+    <PopupState variant="popover" popupId="demo-popup-popover">
+      {popupState => (
+        <Box>
+          <Button {...bindTrigger(popupState)}>
+            <VideoFile />
+          </Button>
+          <Popover
+            {...bindPopover(popupState)}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+          >
+            <video
+              controls
+              style={{ maxWidth: '100%', maxHeight: '200px' }}
+              src={`data:video/mp4;base64,${recordingResult.base64_video}`}
+            >
+              <track kind="captions" srcLang="en" label="English captions" />
+              Your browser does not support the video tag.
+            </video>
+          </Popover>
+        </Box>
+      )}
+    </PopupState>
   );
 }
