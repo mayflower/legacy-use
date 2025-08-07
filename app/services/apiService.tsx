fc599ca4115d19ad123093714b3459ebccbb06dc
Richard <richard@stromer.org>
2025-08-07 16:35:05 +0200

    fix api definition create and edit

--- app/services/apiService.tsx
+++ app/services/apiService.tsx
@@ -25,6 +25,7 @@ import {
   type HttpExchangeLog,
   hardDeleteSessionSessionsSessionIdHardDelete,
   hardDeleteTargetTargetsTargetIdHardDelete,
+  type ImportApiDefinitionBody,
   type ImportApiDefinitionRequest,
   importApiDefinitionApiDefinitionsImportPost,
   interruptJobTargetsTargetIdJobsJobIdInterruptPost,
@@ -159,8 +160,8 @@ export const exportApiDefinition = async (apiName: string) => {
   return exportApiDefinitionApiDefinitionsApiNameExportGet(apiName);
 };
 
-export const importApiDefinition = async (apiDefinition: ImportApiDefinitionRequest) => {
-  return importApiDefinitionApiDefinitionsImportPost(apiDefinition);
+export const importApiDefinition = async (apiDefinition: ImportApiDefinitionBody) => {
+  return importApiDefinitionApiDefinitionsImportPost({ api_definition: apiDefinition });
 };
 
 export const getApiDefinitionDetails = async (apiName: string) => {
@@ -195,9 +196,9 @@ export const getApiDefinitionVersion = async (apiName: string, versionId: string
 
 export const updateApiDefinition = async (
   apiName: string,
-  apiDefinition: ImportApiDefinitionRequest,
+  apiDefinition: ImportApiDefinitionBody,
 ) => {
-  return updateApiDefinitionApiDefinitionsApiNamePut(apiName, apiDefinition);
+  return updateApiDefinitionApiDefinitionsApiNamePut(apiName, { api_definition: apiDefinition });
 };
 
 export const archiveApiDefinition = async (apiName: string) => {
