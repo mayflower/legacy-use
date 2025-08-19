364a23f3e7a29dd885045288922131cfa333a93c
Richard <richard@stromer.org>
2025-08-19 20:22:02 +0200

    complete clerk setup

--- app/cloud/index.tsx
+++ app/cloud/index.tsx
@@ -1,3 +1,23 @@
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
 export default function CloudApp() {
-  return <div>Cloud</div>;
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <div>
+        <h1>Cloud</h1>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </ClerkProvider>
+  );
 }
