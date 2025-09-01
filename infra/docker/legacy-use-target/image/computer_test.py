e22d201f0e19877e548f6327f300bde3f942ab03
Richard <richard@stromer.org>
2025-09-01 14:20:25 +0200

    prepare test case for chunking with newline

--- infra/docker/legacy-use-target/image/computer_test.py
+++ infra/docker/legacy-use-target/image/computer_test.py
@@ -5,3 +5,7 @@ def test_chunks():
     assert chunks('Hello, world!', 5) == ['Hello', ', wor', 'ld!']
     assert chunks('Hello, world!', 10) == ['Hello, wor', 'ld!']
     assert chunks('Hello, world!', 100) == ['Hello, world!']
+
+
+def test_chunks_newline():
+    assert chunks('1\n2', 100) == ['1', '2']
