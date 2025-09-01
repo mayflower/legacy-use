253daa3c124e8fd454bd84d0472d2d350e19bc5f
Richard <richard@stromer.org>
2025-09-01 14:43:14 +0200

    add advanced test cases for chunk with newlines

--- infra/docker/legacy-use-target/image/computer_test.py
+++ infra/docker/legacy-use-target/image/computer_test.py
@@ -8,4 +8,25 @@ def test_chunks():
 
 
 def test_chunks_newline():
-    assert chunks('1\n2', 100) == ['1', '2']
+    assert chunks('1\n2', 100) == ['1', '\n', '2']
+    assert chunks('1\n2\n3\n4', 100) == ['1', '\n', '2', '\n', '3', '\n', '4']
+
+
+def test_chunks_newline_and_space():
+    assert chunks('12345\n67890', 5) == [
+        '12345',
+        '\n',
+        '67890',
+    ]
+
+
+def test_chunks_newline_and_space_overlap():
+    assert chunks('12345\n6789\n0', 3) == [
+        '123',
+        '45',
+        '\n',
+        '678',
+        '9',
+        '\n',
+        '0',
+    ]
