2fabf453aaa39934c0a3ec845a41cba4c9b859a7
Richard <richard@stromer.org>
2025-09-23 13:50:27 +0200

    fix redirect to right port and http with local.legacy-use.com

--- app/cloud/components/CreateNewTenant.tsx
+++ app/cloud/components/CreateNewTenant.tsx
@@ -50,7 +50,7 @@ function inferTenantDetails(name: string): InferredTenant {
 }
 
 function handleForwardToNewTenant(host: string, api_key: string) {
-  if (host.includes('localhost')) {
+  if (host.includes('localhost') || host.includes('local.legacy-use.com')) {
     window.open(`http://${host}:${window.location.port}?api_key=${api_key}`, '_blank');
   } else {
     window.open(`https://${host}?api_key=${api_key}`, '_blank');
