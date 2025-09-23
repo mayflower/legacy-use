2482cf32240824b679c51496163006926ec568e3
Richard <richard@stromer.org>
2025-09-23 12:46:07 +0200

    ruff format

--- server/migrations/versions/2478611410c3_add_clerk_user_id.py
+++ server/migrations/versions/2478611410c3_add_clerk_user_id.py
@@ -5,6 +5,7 @@ Revises: 6d173f57c620
 Create Date: 2025-09-23 12:43:27.697355
 
 """
+
 from typing import Sequence, Union
 
 import sqlalchemy as sa
@@ -21,12 +22,19 @@ depends_on: Union[str, Sequence[str], None] = None
 
 @for_each_tenant_schema
 def upgrade(schema: str) -> None:
-    op.add_column('tenants', sa.Column('clerk_user_id', sa.String(length=256), nullable=True), schema='shared')
-    op.create_unique_constraint('unique_clerk_user_id', 'tenants', ['clerk_user_id'], schema='shared')
-
+    op.add_column(
+        'tenants',
+        sa.Column('clerk_user_id', sa.String(length=256), nullable=True),
+        schema='shared',
+    )
+    op.create_unique_constraint(
+        'unique_clerk_user_id', 'tenants', ['clerk_user_id'], schema='shared'
+    )
 
 
 @for_each_tenant_schema
 def downgrade(schema: str) -> None:
-    op.drop_constraint('unique_clerk_user_id', 'tenants', schema='shared', type_='unique')
+    op.drop_constraint(
+        'unique_clerk_user_id', 'tenants', schema='shared', type_='unique'
+    )
     op.drop_column('tenants', 'clerk_user_id', schema='shared')
