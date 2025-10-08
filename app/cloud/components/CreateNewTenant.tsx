8a71ab55e680ae56a0d31ead135662858a591247
Thilo Reintjes <thilo.reintjes@gmail.com>
2025-10-08 09:40:39 +0200

    Fix: Format

--- app/cloud/components/CreateNewTenant.tsx
+++ app/cloud/components/CreateNewTenant.tsx
@@ -51,7 +51,10 @@ function inferTenantDetails(name: string): InferredTenant {
 
 function handleForwardToNewTenant(host: string, api_key: string) {
   if (host.includes('localhost') || host.includes('local.legacy-use.com')) {
-    window.open(`http://${host}:${window.location.port}?api_key=${api_key}&showWizard=true`, '_blank');
+    window.open(
+      `http://${host}:${window.location.port}?api_key=${api_key}&showWizard=true`,
+      '_blank',
+    );
   } else {
     window.open(`https://${host}?api_key=${api_key}&showWizard=true`, '_blank');
   }
