406505ed9a08eb459f5e443288eab21a9f881c00
Jo Brunner <jo.brunner@mayflower.de>
2025-10-20 16:33:03 +0200

    Fixes docker compose recreation: using the default profile

--- mf/mf-deploy.sh
+++ mf/mf-deploy.sh
@@ -7,9 +7,6 @@ SECRETS_DIR="$PROJECT_ROOT/secrets"
 SSH_PRIVATE_KEY=$AZ_DEMO_VM_SSH_KEY
 VM_USER=$AZ_DEMO_VM_USER
 
-# echo $SCRIPT_DIR
-# exit 0
-
 # Check for SSH private key
 if [ ! -f $SSH_PRIVATE_KEY ]; then
   echo "Error: The private SSH key ${SSH_PRIVATE_KEY} is missing. Please install the private key with this name! You can find the key in Vaultwarden"
@@ -28,12 +25,13 @@ VM_IP=$(az vm show \
 scp -i ${SSH_PRIVATE_KEY} ${SCRIPT_DIR}/docker-compose.yml ${VM_USER}@${VM_IP}:/srv/app/docker-compose.yml
 sops -d ${SECRETS_DIR}/demo-vm.env | ssh -i ${SSH_PRIVATE_KEY} ${VM_USER}@${VM_IP} "cat > /srv/app/.env"
 ssh -i ${SSH_PRIVATE_KEY} ${VM_USER}@${VM_IP} "cd /srv/app && chown ${VM_USER}:docker docker-compose.yml .env"
+
 # login with system managed identity
 # login into container registry
 # recreate docker compose stack with fresh pulled images
-    # az login --identity && \ können wir uns das sparen?
 ssh -i ${SSH_PRIVATE_KEY} ${VM_USER}@${VM_IP} \
   "cd /srv/app && \
+    az login --identity && \
     az acr login -n ${AZ_ACR_NAME} && \
     docker compose --profile pull-only up --pull always --no-start && \
-    docker compose up -d --force-recreate --pull always"
+    docker compose --profile default up -d --force-recreate --pull always"
